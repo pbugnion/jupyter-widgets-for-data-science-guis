c12493b141126145b174698d99010d72314ed865
Pascal Bugnion <pascal@bugnion.org>
2017-10-28 08:37:46 +0100

    Flake8 fixes

--- transclass/labeller.py
+++ transclass/labeller.py
@@ -5,7 +5,7 @@ from . import Classifier, Transaction
 
 
 class Labeller(object):
-    
+
     def __init__(self, transactions, categories):
         self.transactions = transactions
         self.categories = categories
@@ -17,30 +17,33 @@ class Labeller(object):
             layout={'height': '30em', 'overflow_y': 'scroll'}
         )
         self.retrain_button = None
-        
+
     def _train(self):
-        training_set = [transaction for transaction in self.transactions if transaction.category]
+        training_set = [
+            transaction for transaction in self.transactions
+            if transaction.category
+        ]
         self.classifier.train(training_set, self.categories)
-        
+
     def _render_row(self, transaction, prediction):
         [predicted_category, probability] = prediction
         return widgets.HBox([
             widgets.Label(transaction.memo, layout={'width': '300px'}),
             widgets.Dropdown(
-                options=[''] + self.categories, 
-                value=transaction.category, 
+                options=[''] + self.categories,
+                value=transaction.category,
                 layout={'width': '200px'}
             ),
             widgets.Label(predicted_category, layout={'width': '100px'}),
             widgets.FloatProgress(
-                value=probability, 
-                min=1./3., 
-                max=1.0, 
+                value=probability,
+                min=1./3.,
+                max=1.0,
                 description='{:.3f}'.format(probability),
                 layout={'width': '400px'}
             ),
         ], layout={'flex': '1 0 auto'})
-    
+
     def _get_updated_transactions(self):
         transactions = []
         for row_widget in self.transaction_container.children:
@@ -50,7 +53,7 @@ class Labeller(object):
             )
             transactions.append(transaction)
         return transactions
-    
+
     def _render_headers(self):
         return widgets.HBox([
             widgets.HTML('<b>Memo</b>', layout={'width': '300px'}),
@@ -58,12 +61,12 @@ class Labeller(object):
             widgets.HTML('<b>Predicted</b>', layout={'width': '200px'}),
             widgets.HTML('<b>Probability</b>', layout={'width': '100px'})
         ])
-    
+
     def _render_controls(self):
         self.retrain_button = widgets.Button(description='retrain')
         self.retrain_button.on_click(lambda evt: self.retrain())
         return widgets.HBox([self.retrain_button])
-    
+
     def retrain(self):
         # disable retrain button while retraining
         if self.retrain_button is not None:
@@ -75,17 +78,21 @@ class Labeller(object):
         if self.retrain_button is not None:
             self.retrain_button.description = 'retrain'
             self.retrain_button.disabled = False
-        
+
     def render(self):
         predictions = self.classifier.predict_proba(self.transactions)
         row_widgets = []
         zipped_transaction_predictions = zip(self.transactions, predictions)
-        sorted_transaction_predictions = sorted(zipped_transaction_predictions, key=lambda entry: entry[1][1])
+        sorted_transaction_predictions = sorted(
+            zipped_transaction_predictions, key=lambda entry: entry[1][1])
         for (transaction, prediction) in sorted_transaction_predictions:
             row_widget = self._render_row(transaction, prediction)
             row_widgets.append(row_widget)
         headers = self._render_headers()
         controls = self._render_controls()
         self.transaction_container.children = row_widgets
-        self.container.children = [headers, self.transaction_container, controls]
+        self.container.children = [
+            headers, self.transaction_container, controls
+        ]
         return self.container
+
